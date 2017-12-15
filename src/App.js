4f0f3ae14ce8acd136b87699ac64fedcab61e9d5
Stewart Wilson <wilson.x2rt@gmail.com>
2017-12-15 13:57:39 +0100

    Adding partialy styled clock component and initial implementation of background image. Adding normalize.css to dependencies package and importing in index.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Clock from './Clock';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Clock />
       </div>
     );
   }
