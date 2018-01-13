29488b7fb23c22ec5759051f00f9502b0df1b4dc
Stewart Wilson <wilson.x2rt@gmail.com>
2018-01-13 15:23:19 +0100

    todo and links toggle class

--- src/Links.js
+++ src/Links.js
@@ -5,18 +5,25 @@ class Links extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      active: false
+      visible: 'links-popup-container-hidden'
     }
   }
 
   toggleClass = () => {
-    
+    let visible = {...this.state.visible};
+    if(this.state.visible === 'links-popup-container-hidden') {
+      visible = 'links-popup-container';
+      this.setState({visible});
+    } else {
+      visible = 'links-popup-container-hidden';
+      this.setState({visible});
+    }
   }
 
   render() {
     return (
       <div className="react-parent">
-        <div className="links-popup-container">
+        <div className={this.state.visible}>
           <h1>some other thex</h1>
         </div>
         <button onClick={this.toggleClass} className="links-btn">Links</button>
--- src/ToDo.js
+++ src/ToDo.js
@@ -2,13 +2,31 @@ import React from 'react';
 import './ToDo.css';
 
 class ToDo extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      visible: 'todo-popup-container-hidden'
+    }
+  }
+
+  toggleClass = () => {
+    let visible = {...this.state.visible};
+    if(this.state.visible === 'todo-popup-container-hidden') {
+      visible = 'todo-popup-container';
+      this.setState({visible});
+    } else {
+      visible = 'todo-popup-container-hidden';
+      this.setState({visible});
+    }
+  }
+
   render () {
     return (
       <div className="todo-wrapper">
-        <div className="todo-popup-container">
+        <div className={this.state.visible}>
           <h1>some other thex</h1>
         </div>
-        <button className="todo-btn">ToDo</button>
+        <button onClick={this.toggleClass} className="todo-btn">ToDo</button>
       </div>
     )
   }
