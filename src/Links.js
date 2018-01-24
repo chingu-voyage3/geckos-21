d1a725d698024a0bd9d1c9e76486b088ad410c82
Stewart Wilson <wilson.x2rt@gmail.com>
2018-01-24 20:43:47 +0100

    build

--- src/Links.js
+++ src/Links.js
@@ -26,9 +26,9 @@ class Links extends React.Component {
     return (
       <div className="react-parent">
         <div className={this.state.visible}>
-          <a href="" target="_blank"><i className="fa fa-google"></i> Google</a>
-          <a href="" target="_blank"><i className="fa fa-github"></i> GitHub</a>
-          <a href="" target="_blank"><i className="fa fa-stack-overflow" aria-hidden="true"></i> Stack Overflow</a>
+          <a href="https://www.google.com/" target="_blank"><i className="fa fa-google"></i> Google</a>
+          <a href="https://github.com/" target="_blank"><i className="fa fa-github"></i> GitHub</a>
+          <a href="https://stackoverflow.com/" target="_blank"><i className="fa fa-stack-overflow" aria-hidden="true"></i> Stack Overflow</a>
         </div>
         <button onClick={this.toggleClass} className="links-btn">Links</button>
       </div>
